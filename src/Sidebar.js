609c2c6c80fffe6b1f081e95f6007ab495443cb5
Negin <negin.khademi@gmail.com>
2024-12-31 23:43:17 +0330

    sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,40 @@
+import { FaTimes } from 'react-icons/fa';
+import subLinks from './data';
+import { useGlobalContext } from './Contex';
+
 const Sidebar = () => {
-  return <h2>Sidebar</h2>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {subLinks.map((item) => {
+            const { links, page, pageId } = item;
+
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link;
+                    return (
+                      <a href={url} key={id}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
