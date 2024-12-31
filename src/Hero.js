77d27a228271ee98a658fe83c84d0bcaa6f689cb
Negin <negin.khademi@gmail.com>
2024-12-31 21:30:53 +0330

    hero component

--- src/Hero.js
+++ src/Hero.js
@@ -1,5 +1,14 @@
-const Submenu = () => {
-  return <h2>Hero</h2>;
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>Strapi is the leading open-source headless CMS. It’s 100% JavaScript and fully customizable.</p>
+      </div>
+    </div>
+  );
 };
 
-export default Submenu;
+export default Hero;
