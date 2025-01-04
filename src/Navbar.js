a4f0c90cb25f29e902baac384049eaf05fc6d13b
Negin <negin.khademi@gmail.com>
2025-01-04 15:29:47 +0330

    hide submenu

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,15 @@ import Navlinks from './Navlinks';
 import { useGlobalContext } from './Context';
 
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    // console.log(e.target);
+    if (!e.target.classList.contains('nav-link')) {
+      setPageId(null);
+    }
+  };
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <h3 className="logo">strapi</h3>
         <button className="toggle-btn" onClick={openSidebar}>
--- src/Submenu.js
+++ src/Submenu.js
@@ -1,22 +1,41 @@
 import sublinks from './data';
 import { useGlobalContext } from './Context';
+import { useRef } from 'react';
 
 const Submenu = () => {
-  const { pageId } = useGlobalContext();
-  const currentpage = sublinks.find((item) => item.pageId === pageId);
+  const { pageId, setPageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
 
+  const submenuContainer = useRef(null);
+
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = event;
+
+    if (clientX < left + 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+  };
   return (
-    <div className={currentpage ? 'submenu show-submenu' : 'submenu'}>
-      <h5>{currentpage?.page}</h5>
+    <div
+      className={currentPage ? 'submenu show-submenu' : 'submenu'}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
+      <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
-        style={{ gridTemplateColumns: currentpage?.links?.length > 3 ? '1fr 1fr' : '1fr' }}
+        style={{
+          gridTemplateColumns: currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
       >
-        {currentpage?.links?.map((link) => {
+        {currentPage?.links?.map((link) => {
           const { id, url, label, icon } = link;
           return (
             <a key={id} href={url}>
-              {icon} {label}
+              {icon}
+              {label}
             </a>
           );
         })}
@@ -24,5 +43,4 @@ const Submenu = () => {
     </div>
   );
 };
-
 export default Submenu;
